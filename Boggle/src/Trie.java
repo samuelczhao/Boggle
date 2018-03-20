7dcc952edcba1827ca2fbedfbad1d93e1720e962
samuelzhao <samuelzhao@10.104.41.177>
2018-03-20 15:25:08 -0700

    asdf

--- Boggle/src/Trie.java
+++ Boggle/src/Trie.java
@@ -1,6 +1,6 @@
 public class Trie
 {
-	private static final int R = 26;        // extended ASCII
+	private static final int R = 256;        // extended ASCII
 
     private Node root = new Node();
 
